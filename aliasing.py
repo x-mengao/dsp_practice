d7c76ca403c3512178542d758afb7afac59badf5
Xiaomeng Gao <xiaomenggao@gmail.com>
2025-05-14 00:01:57 -0400

    add and modify aliasing related script

--- aliasing.py
+++ aliasing.py
@@ -12,7 +12,7 @@ from scipy.fft import fft, fftfreq
 
 def subplot_label_tool_time(subplot, label):
     subplot.set_title(label)
-    subplot.set_xlabel('Time [s]')
+    subplot.set_xlabel('Time [ms]')
     subplot.set_ylabel('Amplitude')
 
 def subplot_label_tool_freq(subplot, label):
@@ -25,11 +25,13 @@ def run_fft(signal, fs):
     X = fft(signal)
     X_magnitude = np.abs(X) / N  # Normalize the magnitude
     frequencies = fftfreq(N, 1/fs)
-    return frequencies, X_magnitude
+    peak = np.argmax(X_magnitude)
+    return frequencies, X_magnitude, peak
 
+# Signal parameters and sampling frequency options
 freq_sig = 500
 fs1 = 600  # Sampling frequency 1
-fs2 = 1000  # Sampling frequency 2
+fs2 = 800  # Sampling frequency 2
 fs3 = 2000  # Sampling frequency 3
 fs4 = freq_sig * 25  # Sampling frequency 4, 10 times the signal frequency
 nyqust_rate = 2 * freq_sig
@@ -47,40 +49,47 @@ sig_sampled3 = np.sin(2 * np.pi * freq_sig * t3)
 sig_sampled4 = np.sin(2 * np.pi * freq_sig * t4)
 
 # Plotting differences
-fig, ax = plt.subplots(4, 2, figsize=(12, 10))
-ax[0,0].plot(t1, sig_sampled1, label='fs=600 Hz')
+fig, ax = plt.subplots(4, 2, figsize=(10, 8))
+ax[0,0].plot(t1, sig_sampled1, label=f'fs={fs1}Hz')
 ax[0,0].set_xlim(0, 1/freq_sig * 10)
-subplot_label_tool_time(ax[0,0], 'Sampling at 600Hz, showing aliasing effect')
-ax[1,0].plot(t2, sig_sampled2, label='fs=1000 Hz')
+subplot_label_tool_time(ax[0,0], f'Sampling at {fs1}Hz, showing aliasing effect')
+ax[1,0].plot(t2, sig_sampled2, label=f'fs={fs2}Hz')
 ax[1,0].set_xlim(0, 1/freq_sig * 10)
-subplot_label_tool_time(ax[1,0], 'Sampling at 1000Hz, bad aliasing due to exact multiple of signal freq')
-ax[2,0].plot(t3, sig_sampled3, label='fs=2000 Hz')
+subplot_label_tool_time(ax[1,0], f'Sampling at {fs2}Hz, showing aliasing effect')
+ax[2,0].plot(t3, sig_sampled3, label=f'fs={fs3}Hz')
 ax[2,0].set_xlim(0, 1/freq_sig * 10)
-subplot_label_tool_time(ax[2,0], 'Sampling at 2000Hz, no aliasing')
-ax[3,0].plot(t4, sig_sampled4, label='fs=5000 Hz')
+subplot_label_tool_time(ax[2,0], f'Sampling at {fs3}Hz, no aliasing')
+ax[3,0].plot(t4, sig_sampled4, label=f'fs={fs4}Hz')
 ax[3,0].set_xlim(0, 1/freq_sig * 10)
-subplot_label_tool_time(ax[3,0], 'Sampling at 5000Hz, no aliasing')
+subplot_label_tool_time(ax[3,0], f'Sampling at {fs4}Hz, no aliasing')
 
 
 # FFT analysis
-freq1, mag1 = run_fft(sig_sampled1, fs1)
-freq2, mag2 = run_fft(sig_sampled2, fs2)
-freq3, mag3 = run_fft(sig_sampled3, fs3)
-freq4, mag4 = run_fft(sig_sampled4, fs4)
-ax[0,1].plot(freq1, mag1, label='Spetrum of signal sampled at 600Hz')
-ax[0,1].set_xlim(0, 2000)
+freq1, mag1, peak1 = run_fft(sig_sampled1, fs1)
+freq2, mag2, peak2 = run_fft(sig_sampled2, fs2)
+freq3, mag3, peak3 = run_fft(sig_sampled3, fs3)
+freq4, mag4, peak4 = run_fft(sig_sampled4, fs4)
+print(f"Peak1: {freq1[peak1]}Hz, Peak2: {freq2[peak2]}Hz, Peak3: {freq3[peak3]}Hz, Peak4: {freq4[peak4]}Hz")
+
+# Plotting FFT results
+ax[0,1].plot(freq1, mag1, label=f'Spetrum of signal sampled at {fs1}Hz')
+ax[0,1].plot(freq1[peak1], mag1[peak1], 'rx')
+ax[0,1].set_xlim(0, freq_sig * 5)
 subplot_label_tool_freq(ax[0,1], 'FFT of sampled signals')
 
-ax[1,1].plot(freq2, mag2, label='Spectrum of signal sampled at 1000Hz')
-ax[1,1].set_xlim(0, 2000)
+ax[1,1].plot(freq2, mag2, label=f'Spectrum of signal sampled at {fs2}Hz')
+ax[1,1].plot(freq2[peak2], mag2[peak2], 'rx')
+ax[1,1].set_xlim(0, freq_sig * 5)
 subplot_label_tool_freq(ax[1,1], 'FFT of sampled signals')
 
-ax[2,1].plot(freq3, mag3, label='Spectrum of signal sampled at 2000Hz')
-ax[2,1].set_xlim(0, 2000)
+ax[2,1].plot(freq3, mag3, label=f'Spectrum of signal sampled at {fs3}Hz')
+ax[2,1].plot(freq3[peak3], mag3[peak3], 'rx')
+ax[2,1].set_xlim(0, freq_sig * 5)
 subplot_label_tool_freq(ax[2,1], 'FFT of sampled signals')
 
-ax[3,1].plot(freq4, mag4, label='Spectrum of signal sampled at 5000Hz')
-ax[3,1].set_xlim(0, 2000)
+ax[3,1].plot(freq4, mag4, label=f'Spectrum of signal sampled at {fs4}Hz')
+ax[3,1].plot(freq4[peak4], mag4[peak4], 'rx')
+ax[3,1].set_xlim(0, freq_sig * 5)
 subplot_label_tool_freq(ax[3,1], 'FFT of sampled signals')
 
 plt.tight_layout()
---
+++ aliasing_intentional.py
@@ -0,0 +1,109 @@
+# ##############################################################
+# this file explores using alising intentionally to get HR information from a high frequency carrier
+# ##############################################################
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
+from scipy.signal import butter, lfilter
+
+# Signal definition of a HR signal modulated as phase, on carrier radar signal
+def create_radar_signal(f_carrier, fs, f_x, duration, do_plot=False):
+    t = np.arange(0, duration, 1/fs) # Create time samples
+    # Create radar signal with motion modulated phase
+    x = np.cos(2 * np.pi * f_x * t)  # Modulated phase due to heartbeat motion
+    c = 3e8
+    wavelength = c / f_carrier
+    radar_signal = np.cos(2 * np.pi * f_carrier * t + 4 * np.pi * x / wavelength)
+    lo_signal = np.cos(2 * np.pi * f_carrier * t)
+    freqs_radarSig, X_mag_radarSig, peak_radarSig = run_fft(radar_signal, fs)
+
+    if do_plot:
+        fig,ax = plt.subplots(1,2, figsize=(10,6))
+        ax[0].plot(t, radar_signal)
+        ax[1].plot(freqs_radarSig, X_mag_radarSig)
+        ax[1].plot(freqs_radarSig[peak_radarSig], X_mag_radarSig[peak_radarSig], "x")
+        subplot_labels(ax, f'Radar Signal with Modulated Phase at Sample rate {fs/1e9}GHz')
+        print(f"Detected peak:{freqs_radarSig[peak_radarSig]}Hz")
+        # ax[1].set_xlim(0, fs/2)  # Limit x-axis to positive frequencies
+
+    return radar_signal, lo_signal, t 
+
+def mix_signal(radar_signal, lo_signal, t, fs, do_plot=False):
+    # Downconvert the radar signal to baseband
+    mixed_signal = radar_signal * lo_signal
+    freqs_mxed, X_mag_mxed, peak_mxed = run_fft(mixed_signal, fs)
+
+    if do_plot:
+        fig,ax = plt.subplots(1,2, figsize=(10,6))
+        ax[0].plot(t, mixed_signal)
+        ax[1].plot(freqs_mxed, X_mag_mxed)
+        ax[1].plot(freqs_mxed[peak_mxed], X_mag_mxed[peak_mxed], "x")
+        print(f"Detected peak:{freqs_mxed[peak_mxed]}Hz")
+        subplot_labels(ax, f'Mixed Signal at Sample rate {fs/1e9}GHz')
+        # ax[1].set_xlim(0, fs/2)  # Limit x-axis to positive frequencies        
+    return mixed_signal
+
+def subplot_labels(ax, title_timeDomain):
+    ax[0].set_xlabel('Time [s]')
+    ax[0].set_ylabel('Amplitude')
+    ax[0].set_title(title_timeDomain)
+    ax[1].set_xlabel('Frequency [Hz]')
+    ax[1].set_ylabel('Magnitude')
+    ax[1].set_title('FFT Magnitude Spectrum')
+
+def run_lpf(order, f_lo, fs, input_signal, do_plot=False):
+    f_cutoff = f_lo / (fs/2)
+    b, a = butter(order, f_cutoff, 'low')
+    filtered_signal = lfilter(b, a, input_signal)
+
+    if do_plot:
+        plt.figure(figsize=(10,6))
+        plt.plot(filtered_signal)
+        plt.title(f'Fitered Signal')
+        plt.xlabel('Samples')
+        plt.ylabel('Amplitude')
+    return filtered_signal
+
+def run_fft(signal, fs):
+    N = len(signal)
+    X = fft(signal)
+    X_magnitude = np.abs(X) / N  # Normalize the magnitude
+    frequencies = fftfreq(N, 1/fs)
+    peak = np.argmax(X_magnitude)
+    return frequencies, X_magnitude, peak
+
+# Signal definition of a HR signal modulated as phase, on carrier radar signal
+def create_aliased_signal(f_carrier, f_aliased, f_x, duration, do_plot=False):
+    t = np.arange(0, duration, 1/f_aliased) # Create time samples
+    # Create radar signal with motion modulated phase
+    x = np.cos(2 * np.pi * f_x * t)  # Modulated phase due to heartbeat motion
+    c = 3e8
+    wavelength = c / f_carrier
+    radar_signal_aliased = np.cos(2 * np.pi * f_carrier * t + 4 * np.pi * x / wavelength)
+    if_lo_signal = np.cos(2 * np.pi * np.abs(f_carrier - f_aliased) * t)
+    freqs_radarSig_aliased, X_mag_radarSig_aliased, peak_radarSig_aliased = run_fft(radar_signal_aliased, f_aliased)
+
+    if do_plot:
+        fig,ax = plt.subplots(1,2, figsize=(10,6))
+        ax[0].plot(t, radar_signal_aliased)
+        ax[1].plot(freqs_radarSig_aliased, X_mag_radarSig_aliased)
+        ax[1].plot(freqs_radarSig_aliased[peak_radarSig_aliased], X_mag_radarSig_aliased[peak_radarSig_aliased], "x")
+        subplot_labels(ax, f'Radar Signal with Modulated Phase at Sample rate {f_aliased/1e9}GHz')
+        print(f"Detected peak:{freqs_radarSig_aliased[peak_radarSig_aliased]}Hz")
+        # ax[1].set_xlim(0, fs/2)  # Limit x-axis to positive frequencies
+
+    return radar_signal_aliased, if_lo_signal, t
+
+# User defined parameters
+f_carrier = 2.4e9
+sample_rate = f_carrier * 10
+aliased_sample_rate = 2.8e9
+t_duration = 2e-3
+f_hr = 2
+
+# Main functions calling
+radar_signal, lo_signal, t_radarSig = create_radar_signal(f_carrier, sample_rate, f_hr, t_duration, do_plot=True)
+# mixed_signal = mix_signal(radar_signal, lo_signal, t_radarSig, sample_rate, do_plot=True)
+# filtered_signal = run_lpf(4, 100, sample_rate, mixed_signal, do_plot=True)
+radar_signal_aliased, if_lo_signal, t_radarSigAliased = create_aliased_signal(f_carrier, aliased_sample_rate, f_hr, t_duration, do_plot=True)
+mixed_aliased_signal = mix_signal(radar_signal_aliased, if_lo_signal, t_radarSigAliased, aliased_sample_rate, do_plot=True)
